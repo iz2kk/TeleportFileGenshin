43edac4f5e392a9cf24cca897fac0464ec52a41f
iz2kk <hqvblog@gmail.com>
2023-01-20 22:22:21 +0700

    last-update: 20/01/2023 22:22:20,91

--- gitPush.bat
+++ gitPush.bat
@@ -2,9 +2,11 @@
 set mydate=%date%
 set mytime=%time%
 echo on
-git reset
 git add *
 git commit -m "last-update: %mydate% %mytime%"
+
+git pull --rebase izi izilove
+
 git checkout -b izilove
-git push izi izilove
+git push -f izi izilove
 pause.
